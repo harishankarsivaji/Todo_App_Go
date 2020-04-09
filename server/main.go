6800ba08a8e308086c5c0eab26ec020d0a0ddf8e
harishankarsivaji <harishs92@gmail.com>
2020-04-09 23:31:42 +0100

    cleanup

--- server/main.go
+++ server/main.go
@@ -10,8 +10,7 @@ import (
 
 func main() {
 	r := router.Router()
-	// fs := http.FileServer(http.Dir("build"))
-	// http.Handle("/", fs)
+
 	fmt.Println("Starting server on the port 8080...")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
--- server/router/router.go
+++ server/router/router.go
@@ -15,6 +15,6 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
-	router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
+	// router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
 	return router
 }
